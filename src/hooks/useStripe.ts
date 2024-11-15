de3e9d8ada7ecdfc7d6a2109f62a164ff7d69d2f
SrinoYt <136364956+SrinoYt@users.noreply.github.com>
2024-11-15 11:48:43 +0100

    rem: formData from useStripe() hook

--- src/hooks/useStripe.ts
+++ src/hooks/useStripe.ts
@@ -1,15 +1,12 @@
 import {loadStripe} from "@stripe/stripe-js";
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
-
-export function useStripe(action: (formData: FormData) => Promise<string | undefined>) {
+export function useStripe(action: (productId: string, quantity: string) => Promise<string | undefined>) {
     async function buy(productId: string, quantity: number) {
-        const formData = new FormData()
-        formData.append("productId", productId)
-        formData.append("quantity", quantity.toString())
-        const sessionId = await action(formData)
+        const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
+        const sessionId = await action(productId, quantity.toString())
         if (!sessionId) return
+
         const stripe = await stripePromise;
         return stripe?.redirectToCheckout({
             sessionId: sessionId,
